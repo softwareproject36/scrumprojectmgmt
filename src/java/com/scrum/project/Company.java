e0ce3006e2e8a47faf68e8ad20e8fd0bcb3dc004
JeanJacques <JeanJacques@DESKTOP-N8O2L4F>
2018-11-24 14:02:29 +0100

    Gestion Projet

--- src/java/com/scrum/project/Company.java
+++ src/java/com/scrum/project/Company.java
@@ -1,19 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.scrum.project;
 
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
-
-/**
- *
- * @author JeanJacques
- */
-@ManagedBean
-@RequestScoped
 public class Company {
 private String idPers;
 private String company_name;
--- src/java/com/scrum/project/CompanyType.java
+++ src/java/com/scrum/project/CompanyType.java
@@ -5,24 +5,13 @@
  */
 package com.scrum.project;
 
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
-
 /**
  *
  * @author JeanJacques
  */
-@ManagedBean
-@RequestScoped
-public class Company_Type {
+public class CompanyType {
+      private String type;
 
-    private String type;
-    
-    public Company_Type() {
-    }
-public Company_Type(String type) {
-    this.type=type;
-    }
     public String getType() {
         return type;
     }
@@ -30,5 +19,5 @@ public Company_Type(String type) {
     public void setType(String type) {
         this.type = type;
     }
-    
+      
 }
